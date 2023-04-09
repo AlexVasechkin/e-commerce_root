3052bd45cad9c6a203bc23ab2aaa9047af6a62c7
Alex Vasechkin <vasechkin.alex.v@gmail.com>
2023-04-09 15:31:20 +0300

    конфигурирование CACHE_DSN

--- shop_back/dist/src/Application/Services/Redis/RedisClient.php
+++ shop_back/dist/src/Application/Services/Redis/RedisClient.php
@@ -3,13 +3,14 @@
 namespace App\Application\Services\Redis;
 
 use Predis;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class RedisClient
 {
     public Predis\Client $client;
 
-    public function __construct()
+    public function __construct(ParameterBagInterface $parameterBag)
     {
-        $this->client = new Predis\Client('tcp://127.0.0.1:46380');
+        $this->client = new Predis\Client($parameterBag->get('app.cache_dsn'));
     }
 }
